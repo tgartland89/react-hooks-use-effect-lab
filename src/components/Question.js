4587de6470a59a45f2add8795c1849d5ddc62a71
tgartland89 <thomas.gartland@gmail.com>
2023-05-26 13:13:33 -0600

    added useEffect for timer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    return; 
+    }
+    const timerId = setTimeout(() => {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1);
+      }, 1000);
 
-  function handleAnswer(isCorrect) {
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+const { id, prompt, answers, correctIndex } = question;
 
   return (
     <>
